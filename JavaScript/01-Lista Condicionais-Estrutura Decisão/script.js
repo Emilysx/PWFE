c27ef3b654cb16dc35fd7b52cfb7b80dc25e917b
Emilysx <emilyg.oliveira27@gmail.com>
2025-08-08 09:44:19 -0300

    Exercico 2 e 3

--- JavaScript/01-Lista Condicionais-Estrutura Decisão/script.js
+++ JavaScript/01-Lista Condicionais-Estrutura Decisão/script.js
@@ -1,4 +1,4 @@
-// Exercico 1 - Verificar se o número é par/ímpar e múltiplo de 3 ou 5.
+/* // Exercico 1 - Verificar se o número é par/ímpar e múltiplo de 3 ou 5.
 console.log("Exercício 1")
 
 let num = Number(prompt(
@@ -31,3 +31,67 @@ if (num % 3 == 0 && num % 5 == 0) {
     console.log("Esse número não é multiplo 3 e nem de 5!");
     alert("Esse número não é multiplo 3 e nem de 5!");
 }
+*/
+
+/* // Exercicio 2 - Verificar qual numero é maior
+console.log("Exercicio 2")
+let num1 = Number(prompt(
+    "Esse programa vai mostrar qual número é o maior!" +
+    "\nInsira o primeiro número: "
+));
+let num2 = Number(prompt("Insira o segundo número: "));
+let num3 = Number(prompt("Insira o terceiro e último número: "));
+
+if (num1 > num2 && num1 > num3) {
+  console.log(`O número ${num1} é o maior!`);
+  alert(`O número ${num1} é o maior!`);
+
+} else if (num2 > num1 && num2 > num3) {
+  console.log(`O número ${num2} é o maior!`);
+  alert(`O número ${num2} é o maior!`);
+
+} else if (num3 > num1 && num3 > num2) {
+  console.log(`O número ${num3} é o maior!`);
+  alert(`O número ${num3} é o maior!`);
+
+} else {
+  console.log("Há dois ou mais números iguais e maiores.");
+  alert("Há dois ou mais números iguais e maiores.");
+}
+*/
+
+/* // Exercicio 3 - Irá Classificar conforne a idade
+console.log("Exercicio 3")
+let idade = Number(prompt(
+    "Esse prograama vai fazer a classificação conforme a idade!" +
+    "\nInforme sua idade: "
+));
+
+if (idade <= 0) {
+    console.log("Idade inválida!");
+    alert("Idade inválida!");
+}
+else if (idade <= 2) {
+    console.log("Classificação: Bebê");
+    alert("Classificação: Bebê");
+
+} else if (idade > 2 && idade <= 13) {
+    console.log("Classificação: Criança");
+    alert("Classificação: Criança");
+
+} else if (idade > 13 && idade <= 18) {
+    console.log("Classificação: Adolescente");
+    alert("Classificação: Adolescente");
+
+} else if (idade > 18 && idade <= 60) {
+    console.log("Classificação: Adulto");
+    alert("Classificação: Adulto");
+
+} else {
+    console.log("Classificação: Idoso");
+    alert("Classificação: Idoso");
+}
+*/
+
+
+
