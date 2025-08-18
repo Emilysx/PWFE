884fb3e1600d6fb6e79456d0d0ec63660aa9581f
Emily Gabrielle Oliveira <53714206817@>
2025-08-18 14:21:39 -0300

    Aula 03 - 18/08/2025

---
+++ JavaScript/Aula 03/script.js
@@ -0,0 +1,43 @@
+// Arvore dom
+let btn = document.querySelector("button");
+
+function random(num){
+    return Math.floor(Math.random() * (num + 1));
+}
+
+// btn.onclick = function (){
+//     let cor = 
+//     "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
+//     document.body.style.backgroundColor = cor;
+// };
+
+/* function bgChange(){
+    let cor = 
+    "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
+    document.body.style.backgroundColor = cor;
+
+}
+
+btn.addEventListener("click", bgChange);*/
+
+/* // FUNÇÃO ANONIMA
+btn.addEventListener("click", function(){
+    let cor = 
+    "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
+    document.body.style.backgroundColor = cor;
+})
+*/
+
+// ARROW FANCTION 
+// btn.addEventListener("click", ()=> {
+//     let cor = 
+//     "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
+//     document.body.style.backgroundColor = cor;
+// })
+
+function bgChange(event){
+    let cor = 
+    "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
+    event.target.style.backgroundColor = cor;
+}
+btn.addEventListener("click", bgChange);
