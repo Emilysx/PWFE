8b3f844d41b77f2fd62b6971e23255ec812e4ed3
Emily Gabrielle Oliveira <53714206817@>
2025-09-22 14:31:32 -0300

    Primeira aula em react - 22/09/2025

---
+++ React/projeto_01/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css' // import basico, ref para quando foi exportado 
+
+function App() {
+  return(
+    <>
+      <header>
+        <h1> Helou Word</h1>
+      </header>
+    
+    </>
+  )
+}
+
+export default App // Faz que o conponente seja "visto" por todo o React
