4c01fcbcd0ef31c9506c60a2be8ac571e6cd04ec
Emilysx <emilyg.oliveira27@gmail.com>
2026-03-11 14:38:29 -0300

    Inicio do mapa - 11/03/2026

---
+++ 3 - Semestre/senai-burguer/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import './App.css'
+import Mapa from "./componntes/Mapa/Mapa.jsx"
+
+function App() {
+  return(
+    <Mapa />
+  )
+  
+}
+
+export default App
